a91e8ebe755e14bbc6b79dc4e8ff04c5f68b3c2e
Micayla Stern <sternmi7@msu.edu>
2025-10-13 11:06:06 -0400

    ugh

--- Stern_02/js/script.js
+++ Stern_02/js/script.js
@@ -1,5 +1,14 @@
-const demoButton = document.querySelector("button")
+const demoButton = document.querySelector(".clickcount")
+const originalColor = demoButton.style.backgroundColor;
 
 demoButton.addEventListener("click", () => {
 	console.log("Button Was Clicked")
 })
+
+demoButton.addEventListener('click', function() {
+      if (this.style.backgroundColor === originalColor || this.style.backgroundColor === '') {
+        this.style.backgroundColor = 'pink'; // Change to new color
+      } else {
+        this.style.backgroundColor = originalColor; // Revert to original
+      }
+    });
