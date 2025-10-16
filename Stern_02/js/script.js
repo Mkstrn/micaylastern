2a5f85a5c5ce8b5e51688a86f3a5bdd387459634
Micayla Stern <sternmi7@msu.edu>
2025-10-16 00:24:21 -0400

    proj

--- Stern_02/js/script.js
+++ Stern_02/js/script.js
@@ -5,10 +5,35 @@ demoButton.addEventListener("click", () => {
 	console.log("Button Was Clicked")
 })
 
-demoButton.addEventListener('click', function() {
-      if (this.style.backgroundColor === originalColor || this.style.backgroundColor === '') {
-        this.style.backgroundColor = 'pink'; // Change to new color
-      } else {
-        this.style.backgroundColor = originalColor; // Revert to original
-      }
+    let count = 0;
+
+    function incrementCounter() {
+      count++;
+      document.getElementById("count").innerText = count;
+    }
+
+const getUserSelectedText = () => {
+  return window.getSelection().toString();
+}
+
+/* clock */
+
+function updateClock() {
+      const now = new Date();
+      let hours = now.getHours().toString().padStart(2, '0');
+      let minutes = now.getMinutes().toString().padStart(2, '0');
+      let seconds = now.getSeconds().toString().padStart(2, '0');
+
+      const currentTime = `${hours}:${minutes}:${seconds}`;
+      document.getElementById('clock').textContent = currentTime;
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
+
+/* button color change */
+const colorPicker = document.getElementById("bgColorPicker");
+
+    colorPicker.addEventListener("input", function() {
+      document.body.style.backgroundColor = this.value;
     });
